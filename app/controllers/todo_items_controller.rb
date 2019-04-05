fab4d47dd49fbe7d9f1110f0e2686230df7efbd5
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-05 19:10:16 +0000

    adding to controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,22 @@
+
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
+
+
+
 end
