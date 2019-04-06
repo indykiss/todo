771f6d4bab009aa7a3b1218b6ed22cc5714bdd17
indykiss <indirak@alumni.princeton.edu>
2019-04-06 11:12:41 -0400

    added completion for todo items and debugged

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,13 +1,15 @@
 
 
 
+
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
-  end
+   end
 
   def destroy 
     @todo_item = @todo_list.todo_items.find(params[:id])
@@ -19,11 +21,21 @@ class TodoItemsController < ApplicationController
       redirect_to @todo_list
   end 
 
+  def complete
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+   end
+
   private
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+   end
 
   def todo_item_params
     params[:todo_item].permit(:content)
